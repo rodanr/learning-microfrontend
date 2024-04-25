7b6c2bc3205bcce4b632e1904f47f6b598667375
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-25 16:45:57 +0545

    Refactor App component in pdp/src/App.jsx to include Header and Footer components

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
+import Header from "home/Header";
+import Footer from "home/Footer";
+
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: pdp</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">Home Page Content</div>
+    <Footer />
   </div>
 );
+
 ReactDOM.render(<App />, document.getElementById("app"));
