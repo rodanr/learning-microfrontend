2c354950854c9a946bc72863f930167afa8c3052
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-25 16:43:34 +0545

    Update webpack.config.js to expose Header and Footer components in home app

--- home/webpack.config.js
+++ home/webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
@@ -44,7 +44,10 @@ module.exports = (_, argv) => ({
       name: "home",
       filename: "remoteEntry.js",
       remotes: {},
-      exposes: {},
+      exposes: {
+        "./Header": "./src/Header",
+        "./Footer": "./src/Footer",
+      },
       shared: {
         ...deps,
         react: {
@@ -60,6 +63,6 @@ module.exports = (_, argv) => ({
     new HtmlWebPackPlugin({
       template: "./src/index.html",
     }),
-    new Dotenv()
+    new Dotenv(),
   ],
 });
