32dc74de3c1d84c6854b37baaec8708c812e5faa
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-25 16:44:34 +0545

    Refactor App component in home/src/App.jsx to include Header and Footer components

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
+import Header from "./Header";
+import Footer from "./Footer";
+
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: home</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">Home Page Content</div>
+    <Footer />
   </div>
 );
+
 ReactDOM.render(<App />, document.getElementById("app"));
