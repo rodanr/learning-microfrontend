199ad5b3e2520d6281e55ebdf32771cd5f30a14e
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-25 16:42:24 +0545

    Add Header component to home app

---
+++ home/src/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+      Fidget Spinner World
+    </div>
+  );
+}
