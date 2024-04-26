e2dcf4da753c6781ac5eb8f29862223f5e12a642
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-26 14:08:39 +0545

    made prop as object to cause dot access error on remote use

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Header({ appName }) {
+export default function Header({ appMeta }) {
   return (
     <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
-      Fidget Spinner World | {appName}
+      Fidget Spinner World | {appMeta.appName}
     </div>
   );
 }
