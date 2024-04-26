c5d60312a26c6260cc63897cd9c60d76c35c2697
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-26 14:30:28 +0545

    reflect changes from remote component causing error

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -4,18 +4,16 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 
 import { ErrorBoundary } from "react-error-boundary";
-const Header = React.lazy(() => import("home/Header"));
-const Footer = React.lazy(() => import("home/Footer"));
+import Header from "home/Header";
+import Footer from "home/Footer";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
-    <ErrorBoundary fallback={<h1>Something went wrong.</h1>}>
-      <Header />
+    <ErrorBoundary fallback={<h2>Something went wrong!</h2>}>
+      <Header appMeta={{ appName: "PDP" }} />
     </ErrorBoundary>
-    <div className="my-10">Home Page Content</div>
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <Footer />
-    </React.Suspense>
+    <div className="my-10">PDP Page Content</div>
+    <Footer />
   </div>
 );
 
