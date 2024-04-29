4ec0cb58bd25258815c662ce37817569a2aefb0c
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 22:01:39 +0545

    Refactor login function to use fetch API instead of async/await in cart.js

--- cart/src/cart.js
+++ cart/src/cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BehaviorSubject } from "rxjs";
 
 const API_SERVER = "http://localhost:8080";
@@ -46,32 +46,31 @@ export const clearCart = () =>
       getCart();
     });
 
-export const login = async (username, password) => {
-  const response = await fetch(`${API_SERVER}/auth/login`, {
+export const login = (username, password) =>
+  fetch(`${API_SERVER}/auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ username, password }),
-  });
-  const data = await response.json();
-  jwt.next(data.access_token);
-
-  getCart();
-
-  return data.access_token;
-};
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      jwt.next(data.access_token);
+      getCart();
+      return data.access_token;
+    });
 
 export function useLoggedIn() {
-  const [loggedIn, setLoggedIn] = React.useState(!!jwt.value);
-
-  React.useEffect(() => {
+  const [loggedIn, setLoggedIn] = useState(!!jwt.value);
+  useEffect(() => {
     setLoggedIn(!!jwt.value);
-
     return jwt.subscribe((c) => {
       setLoggedIn(!!jwt.value);
     });
   }, []);
-
   return loggedIn;
 }
