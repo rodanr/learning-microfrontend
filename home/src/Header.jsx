e82eecbefb09e7a4d63caba38c4169ab1d5df8b6
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 17:58:24 +0545

    Add MiniCart and Login components to Header.jsx

--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
+import MiniCart from "cart/MiniCart";
+import Login from "cart/Login";
+
 export default function Header({ appMeta }) {
   return (
     <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
-      Fidget Spinner World | {appMeta.appName}
+      <div className="flex">
+        <div className="flex-grow">
+          Fidget Spinner World | {appMeta.appName}
+        </div>
+      </div>
+      <div className="flex-end">
+        <MiniCart />
+        <Login />
+      </div>
     </div>
   );
 }
