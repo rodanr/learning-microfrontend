ab3b8e0616ef0a3ffae0562f5c43c8bf78dac498
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 13:44:23 +0545

    Refactor PDPContent component and add router support

--- pdp/src/PDPContent.jsx
+++ pdp/src/PDPContent.jsx
@@ -1,39 +1,41 @@
 import React from "react";
 
-import {getProductById, currency} from "home/products";
+import { getProductById, currency } from "home/products";
+
+import { useParams } from "react-router-dom";
 
 export default function PDPContent() {
-    const id = 1;
+  const { productId } = useParams();
 
-    const [product, setProduct] = React.useState(null);
+  const [product, setProduct] = React.useState(null);
 
-    React.useEffect(async () => {
-        if (!id) {
-            setProduct(null);
-            return;
-        }
+  React.useEffect(async () => {
+    if (!productId) {
+      setProduct(null);
+      return;
+    }
 
-        const product = await getProductById(id);
-        setProduct(product);
-    }, [id]);
+    const product = await getProductById(productId);
+    setProduct(product);
+  }, [productId]);
 
-    if (!product) return null;
+  if (!product) return null;
 
-    return (
-        <div className="grid grid-cols-2 gap-5">
-            <div>
-                <img src={product.image} alt={product.name}/>
-            </div>
-            <div>
-                <div className="flex">
-                    <h1 className="font-bold text-3xl flex-grow">{product.name}</h1>
-                    <div className="font-bold text-3xl flex-end">
-                        {currency.format(product.price)}
-                    </div>
-                </div>
-                <div className="mt-10">{product.description}</div>
-                <div className="mt-10">{product.longDescription}</div>
-            </div>
+  return (
+    <div className="grid grid-cols-2 gap-5">
+      <div>
+        <img src={product.image} alt={product.name} />
+      </div>
+      <div>
+        <div className="flex">
+          <h1 className="font-bold text-3xl flex-grow">{product.name}</h1>
+          <div className="font-bold text-3xl flex-end">
+            {currency.format(product.price)}
+          </div>
         </div>
-    )
+        <div className="mt-10">{product.description}</div>
+        <div className="mt-10">{product.longDescription}</div>
+      </div>
+    </div>
+  );
 }
