4b2e8ebb7bf10503277846413bc2a5aea5d3dd22
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 17:41:07 +0545

    Add cart functionality to cart.js

--- cart/src/cart.js
+++ cart/src/cart.js
@@ -4,6 +4,47 @@ import { BehaviorSubject } from "rxjs";
 const API_SERVER = "http://localhost:8080";
 
 export const jwt = new BehaviorSubject(null);
+export const cart = new BehaviorSubject(null);
+
+export const getCart = () =>
+  fetch(`${API_SERVER}/cart`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt.value}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      cart.next(res);
+      return res;
+    });
+
+export const addToCart = (id) =>
+  fetch(`${API_SERVER}/cart`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt.value}`,
+    },
+    body: JSON.stringify({ id }),
+  })
+    .then((res) => res.json())
+    .then(() => {
+      getCart();
+    });
+
+export const clearCart = () =>
+  fetch(`${API_SERVER}/cart`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt.value}`,
+    },
+  })
+    .then((res) => res.json())
+    .then(() => {
+      getCart();
+    });
 
 export const login = async (username, password) => {
   const response = await fetch(`${API_SERVER}/auth/login`, {
@@ -15,6 +56,8 @@ export const login = async (username, password) => {
   });
   const data = await response.json();
   jwt.next(data.access_token);
+
+  getCart();
 
   return data.access_token;
 };
