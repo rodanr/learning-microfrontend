9b006c393c429d0f8083bf83e4b8dd54ebe4ee12
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 22:00:44 +0545

    Refactor API calls in products.js for better readability and performance

--- home/src/products.js
+++ home/src/products.js
@@ -1,16 +1,12 @@
-const API_SERVER = 'http://localhost:8080';
+const API_SERVER = "http://localhost:8080";
 
-export const getProducts = async () => {
-    const response = await fetch(`${API_SERVER}/products`);
-    return await response.json();
-}
+export const getProducts = () =>
+  fetch(`${API_SERVER}/products`).then((res) => res.json());
 
-export const getProductById = async (id) => {
-    const response = await fetch(`${API_SERVER}/products/${id}`);
-    return await response.json();
-}
+export const getProductById = (id) =>
+  fetch(`${API_SERVER}/products/${id}`).then((res) => res.json());
 
-export const currency = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+export const currency = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
 });
