a20f89d4104c9e3fce338af57fa6aefb6c882bc4
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 17:00:42 +0545

    remove unused code

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -5,10 +5,5 @@ import { login, jwt } from "./cart";
 export default function CartContent() {
   const [token, setToken] = React.useState("");
 
-  React.useEffect(() => {
-    login("sally", "123");
-    return jwt.subscribe((val) => setToken(val ?? ""));
-  }, []);
-
   return <div>JWT: {token}</div>;
 }
