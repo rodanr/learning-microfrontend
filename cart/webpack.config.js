328ac0f6b71c571b7d6e13bb2c190b89c0283e41
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 17:51:28 +0545

    expose cart components

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -48,7 +48,12 @@ module.exports = (_, argv) => ({
         pdp: "pdp@http://localhost:3001/remoteEntry.js",
         cart: "cart@http://localhost:3002/remoteEntry.js",
       },
-      exposes: {},
+      exposes: {
+        "./cart": "./src/cart.js",
+        "./Login": "./src/Login.jsx",
+        "./MiniCart": "./src/MiniCart.jsx",
+        "./CartContent": "./src/CartContent.jsx",
+      },
       shared: {
         ...deps,
         react: {
