8b66c7888903aa2248aa5db595f5bdd1645e913d
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 22:03:31 +0545

    Refactor Login component to use useState hook for state management

--- cart/src/Login.jsx
+++ cart/src/Login.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { login, useLoggedIn } from "./cart";
 
 export default function Login() {
   const loggedIn = useLoggedIn();
-  const [showLogin, setShowLogin] = React.useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
-  const [username, setUsername] = React.useState("sally");
-  const [password, setPassword] = React.useState("123");
+  const [username, setUsername] = useState("sally");
+  const [password, setPassword] = useState("123");
 
   if (loggedIn) return null;
 
@@ -18,10 +18,11 @@ export default function Login() {
       </span>
       {showLogin && (
         <div
-          className="absolute p-5 border-4 border-blue-800"
+          className="absolute p-5 border-4 border-blue-800 bg-white rounded-xl text-black"
           style={{
             width: 300,
             top: "2rem",
+            left: -250,
           }}
         >
           <input
@@ -29,16 +30,16 @@ export default function Login() {
             placeholder="User Name"
             value={username}
             onChange={(evt) => setUsername(evt.target.value)}
-            className="border text-sm border-gray-400 rounded-md w-full p-1"
+            className="border text-sm border-gray-400 p-2 rounded-md w-full"
           />
           <input
             type="password"
             value={password}
             onChange={(evt) => setPassword(evt.target.value)}
-            className="border text-sm border-gray-400 rounded-md w-full p-1"
+            className="border text-sm border-gray-400 p-2 rounded-md w-full mt-3"
           />
           <button
-            className="bg-green-900 text-white py-2 px-5 rounder-md"
+            className="bg-green-900 text-white py-2 px-5 rounded-md text-sm mt-5"
             onClick={() => login(username, password)}
             id="loginbtn"
           >
