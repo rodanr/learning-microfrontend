16d24286a4b871fabb616abd33d3c3a10bd8277e
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 13:15:09 +0545

    Implement PDPContent

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -3,18 +3,21 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-import { ErrorBoundary } from "react-error-boundary";
+import {ErrorBoundary} from "react-error-boundary";
 import Header from "home/Header";
 import Footer from "home/Footer";
+import PDPContent from "./PDPContent";
 
 const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <ErrorBoundary fallback={<h2>Something went wrong!</h2>}>
-      <Header appMeta={{ appName: "PDP" }} />
-    </ErrorBoundary>
-    <div className="my-10">PDP Page Content</div>
-    <Footer />
-  </div>
+    <div className="text-3xl mx-auto max-w-6xl">
+        <ErrorBoundary fallback={<h2>Something went wrong!</h2>}>
+            <Header appMeta={{appName: "PDP"}}/>
+        </ErrorBoundary>
+        <div className="my-10">
+            <PDPContent/>
+        </div>
+        <Footer/>
+    </div>
 );
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App/>, document.getElementById("app"));
--- pdp/src/PDPContent.jsx
+++ pdp/src/PDPContent.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-export default function PDPContent() {
-    const product = 1;
+import {getProductById, currency} from "home/products";
 
-    return <div>Product {product}</div>
+export default function PDPContent() {
+    const id = 1;
+
+    const [product, setProduct] = React.useState(null);
+
+    React.useEffect(async () => {
+        if (!id) {
+            setProduct(null);
+            return;
+        }
+
+        const product = await getProductById(id);
+        setProduct(product);
+    }, [id]);
+
+    if (!product) return null;
+
+    return (
+        <div className="grid grid-cols-2 gap-5">
+            <div>
+                <img src={product.image} alt={product.name}/>
+            </div>
+        </div>
+    )
 }
