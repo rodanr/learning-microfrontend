670b070719bc697c584bbd4c1d07d39242074ffe
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 22:03:52 +0545

    Update Footer component to remove unnecessary text in Footer.jsx

--- home/src/Footer.jsx
+++ home/src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
-      Only The Best Spinners In The World
+      Only The Best Spinners
     </div>
   );
 }
--- home/src/Header.jsx
+++ home/src/Header.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import MiniCart from "cart/MiniCart";
 import Login from "cart/Login";
 
-export default function Header({ appMeta }) {
+export default function Header() {
   return (
     <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
       <div className="flex">
-        <div className="flex-grow">
-          Fidget Spinner World | {appMeta.appName}
+        <div className="flex-grow flex">
+          <Link to="/">Fidget Spinner World</Link>
+          <div className="mx-5">|</div>
+          <Link id="cart" to="/cart">
+            Cart
+          </Link>
         </div>
-      </div>
-      <div className="flex-end">
-        <MiniCart />
-        <Login />
+        <div className="flex-end relative">
+          <MiniCart />
+          <Login />
+        </div>
       </div>
     </div>
   );
