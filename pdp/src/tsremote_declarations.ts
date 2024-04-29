bfc5d12f066e73848a15202dfb4f6eae094f9baa
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 13:14:48 +0545

    update declaration for home/products module with function declarations

--- pdp/src/tsremote_declarations.ts
+++ pdp/src/tsremote_declarations.ts
@@ -1,4 +1,8 @@
 declare module "home/Header";
 declare module "home/Footer";
 
-declare module "home/products";
+declare module "home/products" {
+    export function getProductById(id: number): Promise<any>;
+
+    export const currency: Intl.NumberFormat
+}
