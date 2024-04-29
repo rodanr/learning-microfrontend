f3a704ce5b5b214c3b994642314a9f4fade53c2b
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 17:10:36 +0545

    add login component

---
+++ cart/src/Login.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { login, useLoggedIn } from "./cart";
+
+export default function Login() {
+  const loggedIn = useLoggedIn();
+  const [showLogin, setShowLogin] = React.useState(false);
+
+  const [username, setUsername] = React.useState("sally");
+  const [password, setPassword] = React.useState("123");
+
+  if (!loggedIn) return null;
+
+  return (
+    <>
+      <span onClick={() => setShowLogin(!showLogin)}>
+        <i className="ri-fingerprint-line text-2xl" id="showlogin"></i>
+      </span>
+      {showLogin && (
+        <div
+          className="absolute p-5 border-4 border-blue-800"
+          style={{
+            width: 300,
+            top: "2rem",
+          }}
+        >
+          <input
+            type="text"
+            placeholder="User Name"
+            value={username}
+            onChange={(evt) => setUsername(evt.target.value)}
+            className="border text-sm border-gray-400 rounded-md w-full p-1"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(evt) => setPassword(evt.target.value)}
+            className="border text-sm border-gray-400 rounded-md w-full p-1"
+          />
+          <button
+            className="bg-green-900 text-white py-2 px-5 rounder-md"
+            onClick={() => login(username, password)}
+            id="loginbtn"
+          >
+            Login
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
