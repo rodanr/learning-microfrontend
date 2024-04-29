eaf1eda4cb4f083e6820e0e5d8953ecf65b9e6ab
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-04-29 22:02:55 +0545

    Refactor PDPContent component to use hooks and improve performance

--- pdp/src/PDPContent.jsx
+++ pdp/src/PDPContent.jsx
@@ -1,23 +1,28 @@
-import React from "react";
-
-import { getProductById, currency } from "home/products";
-
+import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 
+import { getProductById, currency } from "home/products";
+import placeAddToCart from "addtocart/placeAddToCart";
+
 export default function PDPContent() {
-  const { productId } = useParams();
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
 
-  const [product, setProduct] = React.useState(null);
-
-  React.useEffect(async () => {
-    if (!productId) {
+  useEffect(() => {
+    if (id) {
+      getProductById(id).then(setProduct);
+    } else {
       setProduct(null);
-      return;
     }
+  }, [id]);
 
-    const product = await getProductById(productId);
-    setProduct(product);
-  }, [productId]);
+  const addToCart = useRef(null);
+
+  useEffect(() => {
+    if (addToCart.current) {
+      placeAddToCart(addToCart.current, product.id);
+    }
+  }, [product]);
 
   if (!product) return null;
 
@@ -33,6 +38,7 @@ export default function PDPContent() {
             {currency.format(product.price)}
           </div>
         </div>
+        <div ref={addToCart}></div>
         <div className="mt-10">{product.description}</div>
         <div className="mt-10">{product.longDescription}</div>
       </div>
